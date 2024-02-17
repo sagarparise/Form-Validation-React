25f5cf2d54a4495beef37eb7d1466e5920c66d70
sagar parise <sagarparise01@gmail.com>
2024-02-18 00:45:55 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -3,75 +3,112 @@ import "./App.css";
 
 function App() {
   const [email, setEmail] = useState('');
-  const[emailErr, setEmailErr] = useState('');
-  function validateEmail(){
-   
-    // if(email.length == 0)
-    // {
-     
-    //   emailError.innerHTML = 'Email is required';
-    //   return false;
-    // }
-    if(!emailValue.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{1,3}$/))
-    {
-      
-    }
-    else{
+  const[emailErr, setEmailErr] = useState(false);
 
-    }
-    return true;
+  const [password, setPassword] = useState(null);
+  const[passwordErr, setPasswordErr] = useState(false);
+
+  const [confirmPass, setConfirmPass] = useState(null);
+  const[confirmPassErr, setConfirmPassErr] = useState(false);
+
+  function validateEmail(value){
+   
+    setEmail(value);
+    setEmailErr(value.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{1,3}$/));
+   
+   
   }
+
+  function validatePass(value){
+    setPassword(value);
+    setPasswordErr( value.length >= 8);
+  }
+  function  validatePassConfirm(value){
+    setConfirmPass(value);
+    setConfirmPassErr(value === password);
+  }
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+   if(passwordErr && emailErr && confirmPassErr){
+    alert("Successfully submitted");
+    setEmail('')
+    setPassword('')
+    setConfirmPass('');
+    setPasswordErr(false);
+    setEmailErr(false);
+    setConfirmPassErr(false);
+   }
+   else{
+    alert("Please fill all the fields");
+   }
+  }
+
+
   return (
     <>
       <div className="w-full h-screen flex justify-center items-center bg-black">
-        <form action="" className="w-[300px] h-[400px] flex flex-col justify-center gap-5 px-7">
-          <div>
+        <form action="" className="w-[400px] h-[400px] flex flex-col justify-center items-center gap-5 px-7" onSubmit={(e)=>handleSubmit(e)}>
+          <div className="w-full">
             <label
-              for="small-input"
+              htmlFor="email"
               class="block mb-2 text-sm font-medium text-white dark:text-white"
             >
               Email
             </label>
             <input
               type="email"
-              id="small-input"
+              id="email"
               value={email}
-              onChange={(e)=>{
-               setEmail(e.target.value)
-               validateEmail();
+              onChange={(e)=>{        
+               validateEmail(e.target.value);
               }}
-              class="block w-full p-2 text-gray-900 border bg-transparent rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              class={`block w-full p-2 border-2   text-white bg-transparent rounded-lg outline-none ${(emailErr) && 'border-green-600 '}`}
             />
-            <span className=" text-red-700 ps-2">Error</span>
+           {(!emailErr && email) &&  <span className={`font-semibold text-red-700`}> Email is required and must be in a valid format  </span>}
           </div>
 
-          <div>
+          <div className="w-full">
             <label
-              for="small-input"
+              htmlFor="password"
+             
               class="block mb-2 text-sm font-medium text-white dark:text-white"
             >
             Password
             </label>
             <input
               type="password"
-              id="small-input"
-              class="block w-full p-2 text-gray-900 border bg-transparent rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            />
+              id="password"
+              value={password}
+              onChange={(e)=>{
+                validatePass(e.target.value)
+             
+               }}
+              className={`block w-full p-2 text-white border-2 bg-transparent rounded-lg outline-none ${ passwordErr && 'border-green-600'}`}            />
           </div>
-
-          <div>
+          {(!passwordErr && password) && <span className={`font-semibold text-red-700`}>Password must be at least 8 characters long</span>}
+          <div className="w-full">
             <label
-              for="small-input"
+             htmlFor="cPassword"
               class="block mb-2 text-sm font-medium text-white dark:text-white"
             >
              Confirm Password
             </label>
             <input
-              type="email"
-              id="small-input"
-              class="block w-full p-2 text-gray-900 border bg-transparent rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              type="password"
+              id="cPassword"
+              value={confirmPass}
+              onChange={(e)=>{
+                validatePassConfirm(e.target.value);
+               }}
+               className={`block w-full p-2 text-white border-2 bg-transparent rounded-lg outline-none ${confirmPassErr && 'border-green-600'}`}  
             />
+             {(!confirmPassErr && confirmPass) && <span className={`font-semibold text-red-700`}>Password does not match</span>}
           </div>
+          <button
+            type="submit"
+            class="w-40 px-4 py-2 bg-white border font-bold rounded-xl shadow-sm shadow-white"
+          > Sign Up</button>
         </form>
       </div>
     </>
